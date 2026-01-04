ba1407371a5e92bd1e641780f3a94e9cd234e15f
arkiver <arkiver@protonmail.com>
2026-01-04 17:47:56 +0100

    Version 20260104.07. Use TLSv1.3.

--- pipeline.py
+++ pipeline.py
@@ -78,7 +78,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20260104.06'
+VERSION = '20260104.07'
 USER_AGENT = 'Mozilla/5.0 (X11; Linux i686; rv:145.0) Gecko/20100101 Firefox/145.0'
 TRACKER_ID = 'eyeem'
 TRACKER_HOST = 'legacy-api.arpa.li'
@@ -308,7 +308,8 @@ class WgetArgs(object):
             '--warc-header', 'x-wget-at-project-name: ' + TRACKER_ID,
             '--warc-dedup-url-agnostic',
             '--warc-compression-use-zstd',
-            '--warc-zstd-dict-no-include'
+            '--warc-zstd-dict-no-include',
+            '--secure-protocol', 'TLSv1_3'
         ]
         dict_data = ZstdDict.get_dict()
         with open(os.path.join(item['item_dir'], 'zstdict'), 'wb') as f:
